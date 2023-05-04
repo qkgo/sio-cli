79ec29fe0f9a918e7442aa44f2eb1bec093df563
zhy1 <17908471@qq.com>
2023-05-04 11:06:20 +0000

    simply code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use rust_socketio::{SocketBuilder, ClientBuilder, Payload, RawClient};
-use serde_json::json;
-use std::env;
+use rust_socketio::{ClientBuilder, Payload, RawClient};
 use std::time::Instant;
-use tokio::time::sleep;
-use std::time::Duration;
 use structopt::StructOpt;
 use tokio::runtime::Runtime;
-
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "socketio-perf-test")]
@@ -15,43 +10,29 @@ struct Opt {
     path: String,
 }
 
-
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: socketio_perf_test <test_url>");
-        return;
-    }
-
     let opt = Opt::from_args();
     let test_url = opt.path;
 
-    let mut rt = Runtime::new().unwrap();
+    let dial_start = Instant::now();
+    let sio_client = ClientBuilder::new(test_url)
+        .namespace("/admin")
+        .on("connect", |payload: Payload, socket: RawClient| {
+            let connect_duration = dial_start.elapsed();
+            println!("Connection established. Duration: {:?}", connect_duration);
+            Ok(())
+        })
+        .connect()
+        .await;
 
-    // test_url.as_str()
-    rt.block_on(async {
-        let dial_start = Instant::now();
-        let sioClient = ClientBuilder::new(test_url)
-            .namespace("/admin")
-            .on("connect", |payload: Payload, socket: RawClient| {
-                let connect_duration = dial_start.elapsed();
-                println!("Connection established. Duration: {:?}", connect_duration);
-                Ok(());
-            })
-            .connect();
-
-        match sioClient {
-            Ok(sioClient) => {
-                let handshake_duration = dial_start.elapsed();
-                println!("Handshake completed. Duration: {:?}", handshake_duration); 
-            }
-            Err(err) => {
-                println!("Failed to establish a connection: {:?}", err);
-            }
+    match sio_client {
+        Ok(_) => {
+            let handshake_duration = dial_start.elapsed();
+            println!("Handshake completed. Duration: {:?}", handshake_duration);
         }
-    });
-
-
-    // sleep(Duration::from_secs(2)).await;
+        Err(err) => {
+            println!("Failed to establish a connection: {:?}", err);
+        }
+    }
 }
