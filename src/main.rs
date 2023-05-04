72e02c2e41e812abc90c9460671cf1a7b8fb2743
zhy1 <17908471@qq.com>
2023-05-04 12:39:24 +0000

    copy 1c3t3a/rust-socketio  code

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ struct Opt {
     path: String,
 }
 
+
+// https://github.com/1c3t3a/rust-socketio/blob/cb107ba6770fabb109dce173b74ab36ebe039045/socketio/src/client/client.rs#L163
 #[tokio::main]
 async fn main() {
     let opt = Opt::from_args();
@@ -19,11 +21,33 @@ async fn main() {
 
     let start_dial = Instant::now();
     let sio_client = ClientBuilder::new(test_url)
+      .reconnect(true)
+      .max_reconnect_attempts(100)
+      .reconnect_delay(100, 100)
       .transport_type(TransportType::Websocket)
       .namespace("/")
-      .on("connect", move  |_payload: Payload, _socket: RawClient| {
+      
+      .on(Event::Connect, move |_payload: Payload, _socket: RawClient| {
         let connect_duration = start_dial.elapsed().as_millis();
          println!("Connection established. Duration: {:?}", connect_duration);
+        // connect_num_clone.fetch_add(1, Ordering::SeqCst);
+        // let r = socket.emit_with_ack(
+            // "message",
+            // json!(""),
+            // Duration::from_millis(100),
+            // |_, _| {},
+        // );
+        // assert!(r.is_ok(), "should emit message success");
+       }) 
+      // .on("connect", move  |_payload: Payload, _socket: RawClient| {
+        // let connect_duration = start_dial.elapsed().as_millis();
+        //  println!("Connection established. Duration: {:?}", connect_duration);
+      // })
+      
+      .on(Event::Close, move |_, _| {
+        let connect_duration = start_dial.elapsed().as_millis();
+         println!("Connection closed. Duration: {:?}", connect_duration);
+        // close_num_clone.fetch_add(1, Ordering::SeqCst);
       })
       .on_any(|event, payload, _client| {
         if let Payload::String(str) = payload {
