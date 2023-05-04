d7e8e53688506dea589b5df65584e5750f0c0760
zhy1 <17908471@qq.com>
2023-05-04 10:01:28 +0000

    change cli params and add semicolon.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,15 @@ use std::time::Duration;
 use structopt::StructOpt;
 use tokio::runtime::Runtime;
 
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "socketio-perf-test")]
+struct Opt {
+    #[structopt(short, long, help = "Test path")]
+    path: String,
+}
+
+
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,10 +23,13 @@ async fn main() {
         println!("Usage: socketio_perf_test <test_url>");
         return;
     }
-    let test_url = &args[1];
+    // let test_url = &args[1];
     
     // let opt = Opt::from_args();
     // let test_url = format!("{}{}", opt.domain, opt.test_path);
+
+    let opt = Opt::from_args();
+    let url = opt.path;
 
     let mut rt = Runtime::new().unwrap();
 
@@ -28,11 +40,11 @@ async fn main() {
             .on("connect", |payload: Payload, socket: RawClient| {
                 let connect_duration = dial_start.elapsed();
                 println!("Connection established. Duration: {:?}", connect_duration);
-                Ok(())
+                Ok(());
             })
             .on("error",  |err, _| {
                 println!("Error: {}", err);
-                Ok(())
+                Ok(());
             })
             .connect();
 
